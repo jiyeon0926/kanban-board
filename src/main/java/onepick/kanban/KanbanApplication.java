422dfc8c96988eb95f416bc033b023d734fb4dfd
jiyeon0926 <yeoni9094@naver.com>
2024-12-24 18:25:53 +0900

    init

---
+++ src/main/java/onepick/kanban/KanbanApplication.java
@@ -0,0 +1,15 @@
+package onepick.kanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class KanbanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanApplication.class, args);
+    }
+
+}
