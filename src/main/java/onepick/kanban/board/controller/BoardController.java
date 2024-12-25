39ad97f17441b03a6e5f0fa2a5d7a2c4c353b58b
sooyeon_heo <suzi97@naver.com>
2024-12-25 18:42:01 +0900

    feat : 보드 생성 기능 추가

--- src/main/java/onepick/kanban/board/controller/BoardController.java
+++ src/main/java/onepick/kanban/board/controller/BoardController.java
@@ -1,14 +1,29 @@
 package onepick.kanban.board.controller;
 
 import lombok.RequiredArgsConstructor;
+import onepick.kanban.board.dto.BoardResponseDto;
 import onepick.kanban.board.service.BoardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/workspaces/{workspaceId}/boards")
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
+
+    // 보드 생성
+    @PostMapping
+    public ResponseEntity<BoardResponseDto> createBoard(@RequestParam Long workspaceId, @RequestParam String title,
+                                                        @RequestParam(required = false) String backgroundColor,
+                                                        @RequestParam(required = false) String backgroundImage) {
+        BoardResponseDto responseDto = boardService.createBoard(workspaceId, title, backgroundColor, backgroundImage);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
 }
--- src/main/java/onepick/kanban/board/service/BoardService.java
+++ src/main/java/onepick/kanban/board/service/BoardService.java
@@ -1,12 +1,36 @@
 package onepick.kanban.board.service;
 
 import lombok.RequiredArgsConstructor;
+import onepick.kanban.board.dto.BoardRequestDto;
+import onepick.kanban.board.dto.BoardResponseDto;
+import onepick.kanban.board.entity.Board;
 import onepick.kanban.board.repository.BoardRepository;
+import onepick.kanban.card.entity.CardAttachment;
+import onepick.kanban.card.repository.CardRepository;
+import onepick.kanban.workspace.entity.Workspace;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
 
     private final BoardRepository boardRepository;
+    private final CardRepository cardRepository;
+
+    public BoardResponseDto createBoard(Long workspaceId, String title, String backgroundColor, String backgroundImage) {
+        if (title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("제목을 입력해주세요.");
+        }
+        if (workspaceId == null || workspaceId <= 0) {
+            throw new IllegalArgumentException("유효하지 않은 워크스페이스 사용자입니다.");
+        }
+
+        Workspace workspace = new Workspace(workspaceId, "One Pick");
+        Board board = new Board(workspace, title, backgroundColor, backgroundImage);
+        board = boardRepository.save(board);
+
+        return new BoardResponseDto(board.getId(), board.getTitle(), board.getBackgroundColor(),board.getBackgroundImage(), null);
+    }
 }
