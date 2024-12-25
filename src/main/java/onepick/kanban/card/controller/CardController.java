b6ba3bcce7b1e5ac70131894a6857d9dc6cc8df4
sooyeon_heo <suzi97@naver.com>
2024-12-26 04:11:52 +0900

    feat : 카드 생성 기능 추가

--- src/main/java/onepick/kanban/card/controller/CardController.java
+++ src/main/java/onepick/kanban/card/controller/CardController.java
@@ -1,14 +1,28 @@
 package onepick.kanban.card.controller;
 
 import lombok.RequiredArgsConstructor;
+import onepick.kanban.card.dto.CardRequestDto;
+import onepick.kanban.card.dto.CardResponseDto;
 import onepick.kanban.card.service.CardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/boards/{boardId}/lists/{listId}/cards")
 @RequiredArgsConstructor
 public class CardController {
 
     private final CardService cardService;
+
+    // 카드 생성
+    @PostMapping
+    public ResponseEntity<CardResponseDto> createCard(@PathVariable Long boardListId, @RequestBody CardRequestDto requestDto) {
+        CardResponseDto card = cardService.createCard(boardListId, requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(card);
+    }
 }
--- src/main/java/onepick/kanban/card/service/CardService.java
+++ src/main/java/onepick/kanban/card/service/CardService.java
@@ -1,6 +1,12 @@
 package onepick.kanban.card.service;
 
 import lombok.RequiredArgsConstructor;
+import onepick.kanban.boardlist.entity.BoardList;
+import onepick.kanban.boardlist.repository.BoardListRepository;
+import onepick.kanban.card.dto.CardRequestDto;
+import onepick.kanban.card.dto.CardResponseDto;
+import onepick.kanban.card.entity.Card;
+import onepick.kanban.card.repository.CardAttachmentRepository;
 import onepick.kanban.card.repository.CardRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +15,18 @@ import org.springframework.stereotype.Service;
 public class CardService {
 
     private final CardRepository cardRepository;
+    private final CardAttachmentRepository cardAttachmentRepository;
+    private final BoardListRepository boardListRepository;
+
+    public CardResponseDto createCard(Long boardListId, CardRequestDto requestDto) {
+
+        BoardList boardList = boardListRepository.findById(boardListId)
+                .orElseThrow(() -> new IllegalArgumentException("리스트를 찾을 수 없습니다."));
+
+        Card card = new Card(boardList, null, requestDto.getTitle(), requestDto.getContents(), requestDto.getDeadline());
+        card = cardRepository.save(card);
+
+        return new CardResponseDto(card.getId(), card.getTitle(), card.getContents(), card.getDeadline(), null);
+    }
+
 }
