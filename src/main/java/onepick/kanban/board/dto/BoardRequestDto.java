661b507733aa7a0ca3ef337649b00e21afe437ad
sooyeon_heo <suzi97@naver.com>
2024-12-26 04:06:23 +0900

    fix : BoardRequestDto에 @NotBlank 추가

--- src/main/java/onepick/kanban/board/dto/BoardRequestDto.java
+++ src/main/java/onepick/kanban/board/dto/BoardRequestDto.java
@@ -1,5 +1,6 @@
 package onepick.kanban.board.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class BoardRequestDto {
 
+    @NotBlank(message = "제목을 입력해주세요.")
     private String title;
+
     private String backgroundColor;
+
     private String backgroundImage;
 }
