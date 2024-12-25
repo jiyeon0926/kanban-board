07805d0ad289a91937c50c0affbee1855c3ef6a0
sooyeon_heo <suzi97@naver.com>
2024-12-26 04:10:35 +0900

    feat : CardRequestDto, CardResponseDto 추가

--- src/main/java/onepick/kanban/card/dto/CardRequestDto.java
+++ src/main/java/onepick/kanban/card/dto/CardRequestDto.java
@@ -1,7 +1,20 @@
 package onepick.kanban.card.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
+@AllArgsConstructor
 public class CardRequestDto {
+
+    @NotBlank(message = "제목을 입력해주세요.")
+    private final String title;
+
+    @NotBlank(message = "내용을 입력해주세요.")
+    private final String contents;
+
+    private final LocalDateTime deadline;
 }
