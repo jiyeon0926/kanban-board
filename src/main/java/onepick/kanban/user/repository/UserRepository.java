34f6187344b63a4448f6becc58e6166cc928550c
hwiung <khw5233@gmail.com>
2024-12-26 20:18:47 +0900

    feat: 워크스페이스 UserRepository에 findByEmail 메서드 추가

--- src/main/java/onepick/kanban/user/repository/UserRepository.java
+++ src/main/java/onepick/kanban/user/repository/UserRepository.java
@@ -4,12 +4,14 @@ import onepick.kanban.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     default User findByIdOrElseThrow(Long userId) {
         return findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 데이터가 존재하지 않습니다."));
