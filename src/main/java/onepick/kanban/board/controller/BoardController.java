a8d0b3bddb4d92c25741f36c36b740e4c5e17cea
sooyeon_heo <suzi97@naver.com>
2024-12-27 18:19:08 +0900

    feat : BoardController, BoardService에 READONLY 권한 검증 추가 / Board 단건 조회 시 boardList, card 포함 조회

--- src/main/java/onepick/kanban/board/controller/BoardController.java
+++ src/main/java/onepick/kanban/board/controller/BoardController.java
@@ -1,11 +1,14 @@
 package onepick.kanban.board.controller;
 
 import lombok.RequiredArgsConstructor;
+import onepick.kanban.board.dto.BoardDetailResponseDto;
 import onepick.kanban.board.dto.BoardRequestDto;
 import onepick.kanban.board.dto.BoardResponseDto;
 import onepick.kanban.board.service.BoardService;
+import onepick.kanban.user.entity.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,29 +29,48 @@ public class BoardController {
 
     // 보드 생성
     @PostMapping
-    public ResponseEntity<BoardResponseDto> createBoard(@PathVariable Long workspaceId, @RequestBody BoardRequestDto requestDto) {
-        BoardResponseDto board = boardService.createBoard(workspaceId, requestDto);
+    public ResponseEntity<BoardResponseDto> createBoard(@PathVariable Long workspaceId,
+                                                        @RequestBody BoardRequestDto requestDto,
+                                                        @AuthenticationPrincipal User user) {
+        BoardResponseDto board = boardService.createBoard(workspaceId, requestDto, user);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(board);
     }
 
-    // 보드 조회
+    // 보드 다건 조회
     @GetMapping
-    public ResponseEntity<List<BoardResponseDto>> getBoards(@PathVariable Long workspaceId) {
-        List<BoardResponseDto> boards = boardService.getBoards(workspaceId);
+    public ResponseEntity<List<BoardResponseDto>> getBoards(@PathVariable Long workspaceId,
+                                                            @AuthenticationPrincipal User user) {
+        List<BoardResponseDto> boards = boardService.getBoards(workspaceId, user);
+
         return ResponseEntity.ok(boards);
+    }
+
+    // 보드 단건 조회
+    @GetMapping("/{boardId}")
+    public ResponseEntity<BoardDetailResponseDto> getBoard(@PathVariable Long workspaceId,
+                                                           @PathVariable Long boardId,
+                                                           @AuthenticationPrincipal User user) {
+        BoardDetailResponseDto board = boardService.getBoard(workspaceId, boardId, user);
+        return ResponseEntity.ok(board);
     }
 
     // 보드 수정
     @PutMapping("/{boardId}")
-    public ResponseEntity<BoardResponseDto> updateBoard(@PathVariable Long boardId, @RequestBody BoardRequestDto requestDto) {
-        BoardResponseDto board = boardService.updateBoard(boardId, requestDto);
+    public ResponseEntity<BoardResponseDto> updateBoard(@PathVariable Long boardId,
+                                                        @RequestBody BoardRequestDto requestDto,
+                                                        @AuthenticationPrincipal User user) {
+        BoardResponseDto board = boardService.updateBoard(boardId, requestDto, user);
+
         return ResponseEntity.ok(board);
     }
 
     // 보드 삭제
     @DeleteMapping("/{boardId}")
-    public ResponseEntity<Void> deleteBoard(@PathVariable Long boardId) {
-        boardService.deleteBoard(boardId);
+    public ResponseEntity<Void> deleteBoard(@PathVariable Long boardId,
+                                            @AuthenticationPrincipal User user) {
+        boardService.deleteBoard(boardId, user);
+
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/onepick/kanban/boardlist/dto/BoardListResponseDto.java
+++ src/main/java/onepick/kanban/boardlist/dto/BoardListResponseDto.java
@@ -1,17 +1,25 @@
 package onepick.kanban.boardlist.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import onepick.kanban.boardlist.entity.BoardList;
+import onepick.kanban.card.dto.CardResponseDto;
+
+import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class BoardListResponseDto {
     private Long id;
     private String title;
+    private String contents;
     private Integer sequence;
+    private List<CardResponseDto> cards;
 
     public BoardListResponseDto(BoardList boardList) {
         this.id = boardList.getId();
         this.title = boardList.getTitle();
+        this.contents = boardList.getContents();
         this.sequence = boardList.getSequence();
     }
 }
