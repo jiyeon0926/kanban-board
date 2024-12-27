738f90b3a68ad445711375539a1e6624d59b9998
“eunyoung” <jyy714@gmail.com>
2024-12-27 18:24:58 +0900

    fix : BoardListResponseDto에 필드 추가

--- src/main/java/onepick/kanban/boardlist/dto/BoardListResponseDto.java
+++ src/main/java/onepick/kanban/boardlist/dto/BoardListResponseDto.java
@@ -2,12 +2,17 @@ package onepick.kanban.boardlist.dto;
 
 import lombok.Getter;
 import onepick.kanban.boardlist.entity.BoardList;
+import onepick.kanban.card.dto.CardResponseDto;
+
+import java.util.List;
 
 @Getter
 public class BoardListResponseDto {
     private Long id;
     private String title;
+    private String contents;
     private Integer sequence;
+    private List<CardResponseDto> cards;
 
     public BoardListResponseDto(BoardList boardList) {
         this.id = boardList.getId();
