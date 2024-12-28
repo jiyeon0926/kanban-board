aa186deedbb997972249a1d27999f7fed71af1bc
sooyeon_heo <suzi97@naver.com>
2024-12-28 15:23:31 +0900

    fix : CardResponseDto에 List<CardHistoryDto> 추가, List<CommentDto> 주석 처리

--- src/main/java/onepick/kanban/card/dto/CardResponseDto.java
+++ src/main/java/onepick/kanban/card/dto/CardResponseDto.java
@@ -2,6 +2,7 @@ package onepick.kanban.card.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import onepick.kanban.comment.dto.CommentResponseDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,5 +15,7 @@ public class CardResponseDto {
     private final String title;
     private final String contents;
     private final LocalDateTime deadline;
-    private final List<CardAttachmentDto> attachements;
+    private final List<CardAttachmentDto> attachments;
+//    private final List<CommentDto> comments;
+    private final List<CardHistoryDto> histories;
 }
