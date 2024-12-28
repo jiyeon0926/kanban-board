dad580e971364abdff6284da2fc48a2ae3aa0994
sooyeon_heo <suzi97@naver.com>
2024-12-28 15:24:54 +0900

    fix : CardHistoryDto 추가, CardHistoryService의 null 예외 처리 삭제

--- src/main/java/onepick/kanban/card/service/CardHistoryService.java
+++ src/main/java/onepick/kanban/card/service/CardHistoryService.java
@@ -1,24 +1,18 @@
 package onepick.kanban.card.service;
 
+import lombok.RequiredArgsConstructor;
 import onepick.kanban.card.entity.CardHistory;
 import onepick.kanban.card.repository.CardHistoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class CardHistoryService {
     private final CardHistoryRepository cardHistoryRepository;
-
-    public CardHistoryService(CardHistoryRepository cardHistoryRepository) {
-        this.cardHistoryRepository = cardHistoryRepository;
-    }
 
     @Transactional
     public void save(CardHistory cardHistory) {
         cardHistoryRepository.save(cardHistory);
-
-        if (cardHistory != null) {
-            throw new RuntimeException();
-        }
     }
 }
