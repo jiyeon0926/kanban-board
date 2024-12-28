ce59cb9202cbcd88c18c8fada14e74ea05623b91
hwiung <khw5233@gmail.com>
2024-12-29 02:03:20 +0900

    fix: 워크스페이스 및 초대 로직 수정

--- src/main/java/onepick/kanban/workspace/service/WorkspaceService.java
+++ src/main/java/onepick/kanban/workspace/service/WorkspaceService.java
@@ -3,10 +3,6 @@ package onepick.kanban.workspace.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import onepick.kanban.common.SlackNotifier;
-import onepick.kanban.exception.CustomException;
-import onepick.kanban.exception.ErrorCode;
-import onepick.kanban.user.entity.Role;
-import onepick.kanban.user.entity.User;
 import onepick.kanban.user.repository.UserRepository;
 import onepick.kanban.workspace.dto.WorkspaceRequestDto;
 import onepick.kanban.workspace.dto.WorkspaceResponseDto;
@@ -14,6 +10,7 @@ import onepick.kanban.workspace.entity.Workspace;
 import onepick.kanban.workspace.repository.WorkspaceRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -49,9 +46,9 @@ public class WorkspaceService {
     @Transactional
     public void updateWorkspace(Long workspaceId, WorkspaceRequestDto requestDto) {
         Workspace workspace = workspaceRepository.findById(workspaceId)
-                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_WORKSPACE_ID));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "유효하지 않은 워크스페이스 ID입니다."));
         if (requestDto == null || requestDto.getTitle() == null || requestDto.getContents() == null) {
-            throw new CustomException(ErrorCode.WORKSPACE_NOT_FOUND);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "워크스페이스를 찾을 수 없습니다.");
         }
         workspace.update(requestDto.getTitle(), requestDto.getContents());
     }
@@ -59,7 +56,7 @@ public class WorkspaceService {
     @Transactional
     public void deleteWorkspace(Long workspaceId) {
         Workspace workspace = workspaceRepository.findById(workspaceId)
-                .orElseThrow(() -> new CustomException(ErrorCode.WORKSPACE_NOT_FOUND));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "워크스페이스를 찾을 수 없습니다."));
 
         workspaceRepository.delete(workspace);
     }
