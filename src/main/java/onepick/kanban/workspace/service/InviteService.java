9a303679a4300f3ae7710cc16b61029b1da99ec4
jiyeon0926 <yeoni9094@naver.com>
2024-12-30 18:11:14 +0900

    fix: 멤버 초대 예외처리
    중복 초대 못하도록 예외 처리함

--- src/main/java/onepick/kanban/workspace/service/InviteService.java
+++ src/main/java/onepick/kanban/workspace/service/InviteService.java
@@ -8,7 +8,6 @@ import onepick.kanban.user.entity.Role;
 import onepick.kanban.user.entity.User;
 import onepick.kanban.user.repository.UserRepository;
 import onepick.kanban.user.service.MemberService;
-import onepick.kanban.workspace.dto.InviteRequestDto;
 import onepick.kanban.workspace.entity.Invite;
 import onepick.kanban.workspace.entity.Status;
 import onepick.kanban.workspace.entity.Workspace;
@@ -34,43 +33,38 @@ public class InviteService {
     private final MemberService memberService;
     private final SlackNotifier slackNotifier;
 
-    // admin 관리자가 멤버 초대
-    @Transactional
-    public void inviteMembersByAdmin(Long workspaceId, List<String> emails) {
-        Workspace workspace = getWorkspace(workspaceId, emails);
-
-        // 관리자가 초대할 경우, 토큰에 저장된 이메일 가져와서 관리자 찾기
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String authEmail = auth.getName();
-        Optional<User> inviter = userRepository.findByEmail(authEmail);
-
-        // 요청한 이메일을 기반으로 멤버 찾기
-        List<User> invitees = userRepository.findAllByEmailIn(emails);
-
-        List<Invite> invites = invitees.stream()
-                .map(invitee -> new Invite(workspace, inviter.get(), invitee))
-                .collect(Collectors.toList());
-
-        inviteRepository.saveAll(invites);
-
-        String message = inviter.get().getName() + " 관리자가 " + workspace.getTitle() + " 워크스페이스 멤버 초대를 하였습니다.";
-        slackNotifier.sendNotification(message);
-    }
-
     // admin 관리자가 아닌 member 중 워크스페이스 관리자로 지정받은 사람이 멤버 초대
     @Transactional
     public void inviteMembersByStaff(Long workspaceId, List<String> emails) {
-        Workspace workspace = getWorkspace(workspaceId, emails);
+        if (emails.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이메일 목록이 비어있습니다.");
+        }
+
+        Workspace workspace = workspaceRepository.findById(workspaceId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "유효하지 않은 워크스페이스 ID입니다."));
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String authEmail = auth.getName();
         Optional<User> inviter = userRepository.findByEmail(authEmail);
+
+        List<User> invitees = userRepository.findAllByEmailIn(emails);
+        List<Long> userIds = invitees.stream()
+                .map(User::getId)
+                .collect(Collectors.toList());
+
+        List<Invite> existingInvites = inviteRepository.findByWorkspaceIdAndInviteeId(workspaceId, userIds);
+
+        if (!existingInvites.isEmpty()) {
+            List<String> duplicateEmails = existingInvites.stream()
+                    .map(invite -> invite.getInvitee().getEmail())
+                    .collect(Collectors.toList());
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 초대 요청을 보낸 이메일입니다: " + duplicateEmails);
+        }
 
         Optional<Member> member = memberService.findByWorkspaceIdAndUserId(workspaceId, inviter.get().getId());
 
         if (member.get().getRole().equals(Role.STAFF)) {
-            List<User> invitees = userRepository.findAllByEmailIn(emails);
-
             List<Invite> invites = invitees.stream()
                     .map(invitee -> new Invite(workspace, inviter.get(), invitee))
                     .collect(Collectors.toList());
@@ -142,7 +136,47 @@ public class InviteService {
         }
     }
 
-    // 관리자가 초대 취소
+    // admin 관리자가 멤버 초대
+    public void inviteMembersByAdmin(Long workspaceId, List<String> emails) {
+        if (emails.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이메일 목록이 비어있습니다.");
+        }
+
+        Workspace workspace = workspaceRepository.findById(workspaceId).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "유효하지 않은 워크스페이스 ID입니다."));
+
+        // 관리자가 초대할 경우, 토큰에 저장된 이메일 가져와서 관리자 찾기
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String authEmail = auth.getName();
+        Optional<User> inviter = userRepository.findByEmail(authEmail);
+
+        // 요청한 이메일을 기반으로 멤버 찾기
+        List<User> invitees = userRepository.findAllByEmailIn(emails);
+        List<Long> userIds = invitees.stream()
+                .map(User::getId)
+                .collect(Collectors.toList());
+
+        List<Invite> existingInvites = inviteRepository.findByWorkspaceIdAndInviteeId(workspaceId, userIds);
+
+        if (!existingInvites.isEmpty()) {
+            List<String> duplicateEmails = existingInvites.stream()
+                    .map(invite -> invite.getInvitee().getEmail())
+                    .collect(Collectors.toList());
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 초대 요청을 보낸 이메일입니다: " + duplicateEmails);
+        }
+
+        List<Invite> invites = invitees.stream()
+                .map(invitee -> new Invite(workspace, inviter.get(), invitee))
+                .collect(Collectors.toList());
+
+        inviteRepository.saveAll(invites);
+
+        String message = inviter.get().getName() + " 관리자가 " + workspace.getTitle() + " 워크스페이스 멤버 초대를 하였습니다.";
+        slackNotifier.sendNotification(message);
+    }
+
+    // admin 관리자가 초대 취소
     public void deleteInviteByAdmin(Long workspaceId, Long inviteId) {
         Invite invite = inviteRepository.findById(inviteId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "유효하지 않은 초대 ID입니다."));
@@ -152,16 +186,6 @@ public class InviteService {
         }
 
         inviteRepository.delete(invite);
-    }
-
-    private Workspace getWorkspace(Long workspaceId, List<String> emails) {
-        if (emails.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이메일 목록이 비어있습니다.");
-        }
-
-        Workspace workspace = workspaceRepository.findById(workspaceId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "유효하지 않은 워크스페이스 ID입니다."));
-        return workspace;
     }
 
     public boolean isInvitee(User invitee) {
